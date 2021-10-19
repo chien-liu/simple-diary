d1ddb0f5c3c285a96bad5b1560498c46db1c26c3
chien-liu <liu_chien@gapp.nthu.edu.tw>
2021-10-19 23:51:31 +0200

    Add diary decoding function as well as its test program

---
+++ tests/test_upload_content.py
@@ -0,0 +1,17 @@
+import pytest
+import mysql.connector
+from utils.upload_content import _decode
+
+# Type alias
+Database = mysql.connector.connection.MySQLConnection
+
+# @pytest.
+# def dummy_diary() -> Database:
+#     pass
+
+def test_decode_null():
+    filename = "utils/template.txt"
+    date, tags, content = _decode(filename)
+    assert date == ""
+    assert tags == []
+    assert content == ""
---
+++ utils/upload_content.py
@@ -0,0 +1,52 @@
+from typing import Tuple, List
+import mysql.connector
+
+# Type alias
+Database = mysql.connector.connection.MySQLConnection
+
+def upload_content(filename: str, db: Database):
+    date, tags, content = _decode(filename)
+    # Update tables
+    diary_id = _update_diary(db, date, content)
+    tags_id = _update_tag(db, tags)
+    _update_diary_tags(db, diary_id, tags_id)
+
+def _decode(filename: str) -> Tuple[str, List[str], str]:
+    with open(filename) as f:
+        lines = f.readlines()
+        
+        date = lines[2].strip()
+        assert date[:5] == "Date:"
+        if len(date) > len("Date:"):
+            date = date[5:].replace(" ", "")
+        else:
+            date = ""
+
+        tags = lines[3].strip()
+        assert tags[:5] == "Tags:"
+        if len(tags) > len("Tags:"):
+            tags = tags[5:].split(" ")
+        else:
+            tags = []
+
+        content = lines[4]
+        assert content[:8] == "Content:"
+        content = "\n".join(lines[5:])
+    return date, tags, content
+
+def _update_diary(db: Database, date: str, content: str) -> int:
+    cursor = db.cursor()
+    
+    sql = "INSERT INTO diary (date, content) VALUES (%s, %s)"
+    val = (date, content)
+    cursor.excute(sql, val)
+    cursor.commit()
+
+    sql = "SELECT LAST_INSERT_ID() from diary"
+    cursor.excute(sql)
+    id = cursor.fetchall()  # TODO type convert
+    
+    return id
+
+def _update_tags(db: Database, tags: List[str]) -> List[int]:
+    pass
