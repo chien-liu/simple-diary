7170464ce134e2ec90325d7c0cc4a5c7df1abbd5
chien-liu <liu_chien@gapp.nthu.edu.tw>
2021-11-11 23:14:16 +0100

    Add uni-test for update_tag function

---
+++ tests/test_update_mysql_table.py
@@ -0,0 +1,25 @@
+import pytest
+from tests.dummy_mysql_env import DummyMysqlEnv
+from utils.update_mysql_table import update_diary, update_tag
+
+@pytest.fixture
+def content():
+    return "a b 怪怪中\n文字\' ，。\n\n\n1 23"
+
+@pytest.fixture
+def date():
+    return "1993-02-08"
+
+@pytest.fixture
+def tags():
+    return ["a", "BB", "中文"]
+
+# def test_update_diary():
+#     env = DummyMysqlEnv()
+#     cursor = env.database().cursor()
+    
+def test_update_tag(tags):
+    env = DummyMysqlEnv()
+    db = env.database()
+    ids = update_tag(db, tags)
+    assert ids == [1, 2, 3]
